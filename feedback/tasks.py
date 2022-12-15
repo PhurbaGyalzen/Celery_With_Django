6faf00261e003b08e834ff194ca125dfd002945c
Phurba Gyalzen Sherpa <phurba.sherpa@nitv.com.np>
2022-12-15 16:52:38 +0545

    feedback app with email feature added

---
+++ feedback/tasks.py
@@ -0,0 +1,16 @@
+from time import sleep
+from django.conf import settings
+from celery import shared_task
+from django.core.mail import send_mail
+
+
+@shared_task()
+def send_feedback_email_task(email_address, message):
+    """Sends an email when the feedback form has been submitted."""
+    send_mail(
+        subject='FeedBack',
+        message=f'{message}',
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[email_address,]
+    )
+
